af7ae47fb5d467e011b34a83ea1b03366f6e8423
journey-xt <359206421@qq.com>
2019-11-11 18:51:00 +0800

    修改组件

--- src/packDatePicker/RangePicker/RangePicker.tsx
+++ src/packDatePicker/RangePicker/RangePicker.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { Component } from "react";
 import styled from "styled-components";
 import { Row, Col } from "antd";
 import moment from "moment";
 // 声明文件
 import { Moment } from "moment/moment.d";
+import { PickerValue } from "../SingleDatePicker/typeing";
+import { ValueType, ValueStatus } from "../SingleDatePicker/enum";
 import { RangePickerValue } from "./typeing";
 // 组件引用
 import SingleDatePicker from "../SingleDatePicker";
-import { PickerValue } from "../SingleDatePicker/typeing";
-import { ValueStatus, ValueType } from "../SingleDatePicker/enum";
 
 const LayoutCol = styled(Col)`
   position: relative;
@@ -25,7 +25,7 @@ const RelationSpan = styled.div`
 `;
 
 // 声明组件Props类型
-interface Props {
+type Props = {
   disabledDate?: (
     currentDate: Moment | undefined,
     valueStatus?: ValueStatus
@@ -36,268 +36,146 @@ interface Props {
   valueType?: ValueType;
   onChange?: (value: RangePickerValue) => void;
   showToday?: boolean;
-  value?: RangePickerValue;
-}
-
-const RangePicker = (props: Props, ref) => {
-  const { showToday, onChange, value, selectTodayAfter, disabledDate } = props;
-
-  const [RangeValue, setRangeValue] = useState<RangePickerValue>({
-    [ValueStatus.Start]: undefined,
-    [ValueStatus.End]: undefined,
-  });
-
-  // 时间变化回调
-  const rangeChange = useCallback(
-    (value?: PickerValue, valueStatus?: ValueStatus) => {
-      if (onChange) {
-        onChange({
-          ...RangeValue,
-          ...(valueStatus ? { [valueStatus]: value } : {}),
-        });
-      } else {
-        setRangeValue({
-          ...RangeValue,
-          ...(valueStatus ? { [valueStatus]: value } : {}),
-        });
-      }
-    },
-    [onChange, RangeValue]
-  );
-
-  // 不可选择时间回调
-  const rangeDisabledDate = useCallback(
-    (currentDate: Moment | undefined, valueStatus?: ValueStatus) => {
-      // 上层 不可选择时间段 回调
-      if (disabledDate) {
-        return disabledDate(currentDate, valueStatus);
-      }
-
-      const startTime = RangeValue[ValueStatus.Start];
-      const endTime = RangeValue[ValueStatus.End];
-
-      switch (valueStatus) {
-        case ValueStatus.Start:
-          if (selectTodayAfter) {
-            if (currentDate && !endTime) {
-              return currentDate.isBefore(moment());
-            }
-            if (currentDate && endTime) {
-              return (
-                currentDate.isBefore(moment()) || currentDate.isAfter(endTime)
-              );
-            }
-            return false;
-          }
-          return false;
-        case ValueStatus.End:
-          if (selectTodayAfter) {
-            if (currentDate && !startTime) {
-              return currentDate.isBefore(moment());
-            }
-            if (currentDate && startTime) {
-              return currentDate.isBefore(startTime);
-            }
-            return false;
-          }
-          return false;
-        default:
-      }
-      return true;
-    },
-    [RangeValue, disabledDate]
-  );
-
-  // 上层porps变化
-  useEffect(
-    () => {
-      if (value !== undefined) {
-        setRangeValue(value);
-      } else {
-        setRangeValue({
-          [ValueStatus.Start]: undefined,
-          [ValueStatus.End]: undefined,
-        });
-      }
-    },
-    [value]
-  );
-
-  if (!ref) {
-    ref.current = () => {};
-  }
-
-  return (
-    <Row gutter={24}>
-      <LayoutCol span={12}>
-        <SingleDatePicker
-          value={RangeValue[ValueStatus.Start]}
-          valueStatus={ValueStatus.Start}
-          disabledDate={rangeDisabledDate}
-          onChange={rangeChange}
-          showToday={showToday}
-          valueType={ValueType.TimeStamp}
-          defaultPickerValue={
-            RangeValue[ValueStatus.Start]
-              ? moment(RangeValue[ValueStatus.Start])
-              : undefined
-          }
-        />
-      </LayoutCol>
-      <LayoutDiv>
-        <RelationSpan>~</RelationSpan>
-      </LayoutDiv>
-      <Col span={12}>
-        <SingleDatePicker
-          value={RangeValue[ValueStatus.End]}
-          valueStatus={ValueStatus.End}
-          disabledDate={rangeDisabledDate}
-          showToday={showToday}
-          onChange={rangeChange}
-          valueType={ValueType.TimeStamp}
-          defaultPickerValue={
-            RangeValue[ValueStatus.Start]
-              ? moment(RangeValue[ValueStatus.Start])
-              : undefined
-          }
-        />
-      </Col>
-    </Row>
-  );
 };
 
-// class RangePicker extends Component<Props, State> {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       currentDate: moment(),
-//       value: { [ValueStatus.Start]: undefined, [ValueStatus.End]: undefined }, // 内部维护 时间组件的值
-//     };
-//   }
+// 声明组件State类型
+type State = {
+  value: RangePickerValue;
+  currentDate: Moment;
+};
 
-//   static defaultProps = {
-//     valueType: ValueType.TimeStamp,
-//     format: "YYYY-MM-DD",
-//     valueStatus: ValueStatus.Start,
-//     selectTodayAfter: true,
-//     showToday: true,
-//   };
+class RangePicker extends Component<Props, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: moment(),
+      value: { [ValueStatus.Start]: undefined, [ValueStatus.End]: undefined }, // 内部维护 时间组件的值
+    };
+  }
 
-//   static getDerivedStateFromProps(props, state) {
-//     const { value } = props;
+  static defaultProps = {
+    valueType: ValueType.TimeStamp,
+    format: "YYYY-MM-DD",
+    valueStatus: ValueStatus.Start,
+    selectTodayAfter: true,
+    showToday: true,
+  };
 
-//     if (value && (value[ValueStatus.Start] || value[ValueStatus.End])) {
-//       return {
-//         value: {
-//           [ValueStatus.Start]: value[ValueStatus.Start],
-//           [ValueStatus.End]: value[ValueStatus.End],
-//         },
-//       };
-//     }
-//     return {
-//       value: { [ValueStatus.Start]: undefined, [ValueStatus.End]: undefined },
-//     };
-//   }
+  static getDerivedStateFromProps(props) {
+    const { value } = props;
 
-//   // 不可选择时间回调
-//   disabledDate = (
-//     currentDate: Moment | undefined,
-//     valueStatus?: ValueStatus
-//   ) => {
-//     const { disabledDate, selectTodayAfter } = this.props;
-//     // 上层 不可选择时间段 回调
-//     if (disabledDate) {
-//       return disabledDate(currentDate, valueStatus);
-//     }
+    if (value && (value[ValueStatus.Start] || value[ValueStatus.End])) {
+      return {
+        value: {
+          [ValueStatus.Start]: value[ValueStatus.Start],
+          [ValueStatus.End]: value[ValueStatus.End],
+        },
+      };
+    }
+    return {
+      value: { [ValueStatus.Start]: undefined, [ValueStatus.End]: undefined },
+    };
+  }
 
-//     const { value, currentDate: compareDate } = this.state;
-//     const startTime = value[ValueStatus.Start];
-//     const endTime = value[ValueStatus.End];
+  // 不可选择时间回调
+  disabledDate = (
+    currentDate: Moment | undefined,
+    valueStatus?: ValueStatus
+  ) => {
+    const { disabledDate, selectTodayAfter } = this.props;
+    // 上层 不可选择时间段 回调
+    if (disabledDate) {
+      return disabledDate(currentDate, valueStatus);
+    }
 
-//     switch (valueStatus) {
-//       case ValueStatus.Start:
-//         if (selectTodayAfter) {
-//           if (currentDate && !endTime) {
-//             return currentDate.isBefore(compareDate);
-//           }
-//           if (currentDate && endTime) {
-//             return (
-//               currentDate.isBefore(compareDate) || currentDate.isAfter(endTime)
-//             );
-//           }
-//           return false;
-//         }
-//         return false;
-//       case ValueStatus.End:
-//         if (selectTodayAfter) {
-//           if (currentDate && !startTime) {
-//             return currentDate.isBefore(compareDate);
-//           }
-//           if (currentDate && startTime) {
-//             return currentDate.isBefore(startTime);
-//           }
-//           return false;
-//         }
-//         return false;
-//       default:
-//     }
-//     return true;
-//   };
+    const { value, currentDate: compareDate } = this.state;
+    const startTime = value[ValueStatus.Start];
+    const endTime = value[ValueStatus.End];
 
-//   //  时间变化回调
-//   onChange = (value?: PickerValue, valueStatus?: ValueStatus) => {
-//     const { onChange } = this.props;
-//     const { value: stateValue } = this.state;
-//     if (onChange) {
-//       onChange({
-//         ...stateValue,
-//         ...(valueStatus ? { [valueStatus]: value } : {}),
-//       });
-//     } else {
-//       this.setState({
-//         value: {
-//           ...stateValue,
-//           ...(valueStatus ? { [valueStatus]: value } : {}),
-//         },
-//       });
-//     }
-//   };
+    switch (valueStatus) {
+      case ValueStatus.Start:
+        if (selectTodayAfter) {
+          if (currentDate && !endTime) {
+            return currentDate.isBefore(compareDate);
+          }
+          if (currentDate && endTime) {
+            return (
+              currentDate.isBefore(compareDate) || currentDate.isAfter(endTime)
+            );
+          }
+          return false;
+        }
+        return false;
+      case ValueStatus.End:
+        if (selectTodayAfter) {
+          if (currentDate && !startTime) {
+            return currentDate.isBefore(compareDate);
+          }
+          if (currentDate && startTime) {
+            return currentDate.isBefore(startTime);
+          }
+          return false;
+        }
+        return false;
+      default:
+    }
+    return true;
+  };
 
-//   render() {
-//     const { value } = this.state;
-//     const startTime = value[ValueStatus.Start];
-//     const endTime = value[ValueStatus.End];
-//     const { showToday } = this.props;
-//     return (
-//       <Row gutter={24}>
-//         <LayoutCol span={12}>
-//           <SingleDatePicker
-//             value={startTime}
-//             valueStatus={ValueStatus.Start}
-//             disabledDate={this.disabledDate}
-//             onChange={this.onChange}
-//             showToday={showToday}
-//             valueType={ValueType.TimeStamp}
-//             defaultPickerValue={startTime ? moment(startTime) : undefined}
-//           />
-//         </LayoutCol>
-//         <LayoutDiv>
-//           <RelationSpan>~</RelationSpan>
-//         </LayoutDiv>
-//         <Col span={12}>
-//           <SingleDatePicker
-//             value={endTime}
-//             valueStatus={ValueStatus.End}
-//             disabledDate={this.disabledDate}
-//             showToday={showToday}
-//             onChange={this.onChange}
-//             valueType={ValueType.TimeStamp}
-//             defaultPickerValue={startTime ? moment(startTime) : undefined}
-//           />
-//         </Col>
-//       </Row>
-//     );
-//   }
-// }
+  //  时间变化回调
+  onChange = (value?: PickerValue, valueStatus?: ValueStatus) => {
+    const { onChange } = this.props;
+    const { value: stateValue } = this.state;
+    if (onChange) {
+      onChange({
+        ...stateValue,
+        ...(valueStatus ? { [valueStatus]: value } : {}),
+      });
+    } else {
+      this.setState({
+        value: {
+          ...stateValue,
+          ...(valueStatus ? { [valueStatus]: value } : {}),
+        },
+      });
+    }
+  };
 
-export default React.forwardRef(RangePicker);
+  render() {
+    const { value } = this.state;
+    const startTime = value[ValueStatus.Start];
+    const endTime = value[ValueStatus.End];
+    const { showToday } = this.props;
+    return (
+      <Row gutter={24}>
+        <LayoutCol span={12}>
+          <SingleDatePicker
+            value={startTime}
+            valueStatus={ValueStatus.Start}
+            disabledDate={this.disabledDate}
+            onChange={this.onChange}
+            showToday={showToday}
+            valueType={ValueType.TimeStamp}
+            defaultPickerValue={startTime ? moment(startTime) : undefined}
+          />
+        </LayoutCol>
+        <LayoutDiv>
+          <RelationSpan>~</RelationSpan>
+        </LayoutDiv>
+        <Col span={12}>
+          <SingleDatePicker
+            value={endTime}
+            valueStatus={ValueStatus.End}
+            disabledDate={this.disabledDate}
+            showToday={showToday}
+            onChange={this.onChange}
+            valueType={ValueType.TimeStamp}
+            defaultPickerValue={startTime ? moment(startTime) : undefined}
+          />
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default RangePicker;
--- src/packDatePicker/SingleDatePicker/SingleDatePicker.tsx
+++ src/packDatePicker/SingleDatePicker/SingleDatePicker.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { PureComponent } from "react";
 import styled from "styled-components";
 import { DatePicker } from "antd";
 import moment from "moment";
 import "moment/locale/zh-cn";
-import { transformMoment, transformTimeStamp } from "../utils";
+// import { transformMoment, transformTimeStamp } from "../utils";
 
 // 声明文件
 import { Moment } from "moment/moment.d";
@@ -27,188 +27,119 @@ export interface SingleDatePickerProps {
     valueStatus?: ValueStatus
   ) => boolean;
   valueType?: ValueType;
-  onChange?: (value?: PickerValue, valueStatus?: ValueStatus) => void;
+  onChange?: (value?: PickerValue, ValueStatus?) => void;
   defaultPickerValue?: Moment;
   showToday?: boolean;
   value?: string | number | Moment | Date;
 }
 
-const SingleDatePicker = (props: SingleDatePickerProps, ref) => {
-  const {
-    // format = "YYYY-MM-DD",
-    valueStatus = ValueStatus.Start,
-    valueType = ValueType.TimeStamp,
-    value,
-    onChange,
-    showToday,
-    defaultPickerValue,
-    disabledDate,
-    selectTodayAfter,
-  } = props;
-
-  const [dateValue, setDateValue] = useState(transformMoment(value));
-
-  // 变化回调
-  const dateChange = useCallback(
-    (date: Moment | null, dateString?: string) => {
-      if (onChange) {
-        switch (valueType) {
-          case ValueType.TimeStamp:
-            return onChange(transformTimeStamp(date, valueStatus), valueStatus);
-          case ValueType.TimeString:
-            return onChange(dateString, valueStatus);
-          case ValueType.Moment:
-          default:
-            return onChange(date, valueStatus);
-        }
-      } else {
-        setDateValue(transformMoment(value));
-      }
-    },
-    [onChange, valueType, valueStatus]
-  );
-
-  // 不可选择时间回调
-  const disabledTime = useCallback(
-    (currentDate: Moment | undefined) => {
-      // 传递外层API 禁用日期
-      if (disabledDate && currentDate) {
-        return disabledDate(currentDate, valueStatus);
-      }
-      if (selectTodayAfter) {
-        if (currentDate) {
-          return currentDate.isBefore(dateValue, "day");
-        }
-        return false;
-      }
-      return false;
-    },
-    [disabledDate, selectTodayAfter, dateValue, valueStatus]
-  );
-
-  useEffect(
-    () => {
-      setDateValue(transformMoment(value));
-    },
-    [value]
-  );
-
-  if (!ref) {
-    ref.current = () => {};
-  }
-
-  return (
-    <PackDataPick
-      value={dateValue}
-      onChange={dateChange}
-      disabledDate={disabledTime}
-      defaultPickerValue={defaultPickerValue}
-      showToday={showToday}
-      //  renderExtraFooter={this.renderExtraFooter}
-    />
-  );
+// 声明组件State类型
+type State = {
+  currentDate: Moment;
+  value?: string | number | Moment | Date;
 };
 
-// class SingleDatePicker extends PureComponent<SingleDatePickerProps, State> {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       currentDate: moment(), // 当前时间
-//       value: props.value, // 内部维护 时间组件的值得
-//     };
-//   }
+class SingleDatePicker extends PureComponent<SingleDatePickerProps, State> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentDate: moment(), // 当前时间
+      value: props.value, // 内部维护 时间组件的值得
+    };
+  }
 
-//   static getDerivedStateFromProps(props, state) {
-//     const { value } = props;
-//     if (value) {
-//       return { value };
-//     }
-//     return { value: undefined };
-//   }
+  static getDerivedStateFromProps(props) {
+    const { value } = props;
+    if (value) {
+      return { value };
+    }
+    return { value: undefined };
+  }
 
-//   static defaultProps = {
-//     valueType: ValueType.TimeStamp,
-//     format: "YYYY-MM-DD",
-//     valueStatus: ValueStatus.Start,
-//     selectTodayAfter: true,
-//   };
+  static defaultProps = {
+    valueType: ValueType.TimeStamp,
+    format: "YYYY-MM-DD",
+    valueStatus: ValueStatus.Start,
+    selectTodayAfter: true,
+  };
 
-//   // 不可选择时间回调
-//   disabledDate = (currentDate: Moment | undefined) => {
-//     const { disabledDate, selectTodayAfter, valueStatus } = this.props;
-//     // 传递外层API 禁用日期
-//     if (disabledDate && currentDate) {
-//       return disabledDate(currentDate, valueStatus);
-//     }
-//     if (selectTodayAfter) {
-//       const { currentDate: compareDate } = this.state;
-//       if (currentDate) {
-//         return currentDate.isBefore(compareDate, "day");
-//       }
-//       return false;
-//     }
-//     return false;
-//   };
+  // 不可选择时间回调
+  disabledDate = (currentDate: Moment | undefined) => {
+    const { disabledDate, selectTodayAfter, valueStatus } = this.props;
+    // 传递外层API 禁用日期
+    if (disabledDate && currentDate) {
+      return disabledDate(currentDate, valueStatus);
+    }
+    if (selectTodayAfter) {
+      const { currentDate: compareDate } = this.state;
+      if (currentDate) {
+        return currentDate.isBefore(compareDate, "day");
+      }
+      return false;
+    }
+    return false;
+  };
 
-//   // 通过传入的 值 转成moment对象传递给组件
-//   transformValue = date => {
-//     const transformDate = moment(date);
-//     if (date && transformDate.isValid()) {
-//       return transformDate;
-//     }
-//     return undefined;
-//   };
+  // 通过传入的 值 转成moment对象传递给组件
+  transformValue = date => {
+    const transformDate = moment(date);
+    if (date && transformDate.isValid()) {
+      return transformDate;
+    }
+    return undefined;
+  };
 
-//   // 根据传递回来的 moment对象 取得 开始时间戳 和结束时间戳
-//   timeStampBack = (date?: Moment, valueStatus?: ValueStatus) => {
-//     const { valueType } = this.props;
-//     if (valueType === ValueType.TimeStamp) {
-//       switch (valueStatus) {
-//         case ValueStatus.Start:
-//           return date ? date.startOf("day").valueOf() : undefined;
-//         case ValueStatus.End:
-//           return date ? date.endOf("day").valueOf() : undefined;
-//         default:
-//           return date ? date.valueOf() : undefined;
-//       }
-//     }
-//   };
+  // 根据传递回来的 moment对象 取得 开始时间戳 和结束时间戳
+  timeStampBack = (date: Moment | null, valueStatus?: ValueStatus) => {
+    const { valueType } = this.props;
+    if (valueType === ValueType.TimeStamp) {
+      switch (valueStatus) {
+        case ValueStatus.Start:
+          return date ? date.startOf("day").valueOf() : undefined;
+        case ValueStatus.End:
+          return date ? date.endOf("day").valueOf() : undefined;
+        default:
+          return date ? date.valueOf() : undefined;
+      }
+    }
+    return undefined;
+  };
 
-//   // 时间变化回调
-//   onChange = (date?: Moment, dateString?: string) => {
-//     const { valueType, valueStatus, onChange } = this.props;
-//     if (onChange) {
-//       switch (valueType) {
-//         case ValueType.TimeStamp:
-//           return onChange(this.timeStampBack(date, valueStatus), valueStatus);
-//         case ValueType.TimeString:
-//           return onChange(dateString, valueStatus);
-//         case ValueType.Moment:
-//         default:
-//           return onChange(date, valueStatus);
-//       }
-//     } else {
-//       this.setState({ value: date });
-//     }
-//   };
+  // 时间变化回调
+  onChange = (date: Moment | null, dateString?: string) => {
+    const { valueType, valueStatus, onChange } = this.props;
+    if (onChange) {
+      switch (valueType) {
+        case ValueType.TimeStamp:
+          return onChange(this.timeStampBack(date, valueStatus), valueStatus);
+        case ValueType.TimeString:
+          return onChange(dateString, valueStatus);
+        case ValueType.Moment:
+        default:
+          return onChange(date, valueStatus);
+      }
+    } else {
+      this.setState({ value: date || undefined });
+    }
+  };
 
-//   // 添加额外的的页脚render
-//   renderExtraFooter = () => <div>9999</div>;
+  // 添加额外的的页脚render
+  renderExtraFooter = () => <div>9999</div>;
 
-//   render() {
-//     const { value } = this.state;
-//     const { defaultPickerValue, showToday } = this.props;
-//     return (
-//       <PackDataPick
-//         value={this.transformValue(value)}
-//         onChange={this.onChange}
-//         disabledDate={this.disabledDate}
-//         defaultPickerValue={defaultPickerValue}
-//         showToday={showToday}
-//         //  renderExtraFooter={this.renderExtraFooter}
-//       />
-//     );
-//   }
-// }
+  render() {
+    const { value } = this.state;
+    const { defaultPickerValue, showToday } = this.props;
+    return (
+      <PackDataPick
+        value={this.transformValue(value)}
+        onChange={this.onChange}
+        disabledDate={this.disabledDate}
+        defaultPickerValue={defaultPickerValue}
+        showToday={showToday}
+        //  renderExtraFooter={this.renderExtraFooter}
+      />
+    );
+  }
+}
 
-export default React.forwardRef(SingleDatePicker);
+export default SingleDatePicker;
